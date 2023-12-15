e9f766f2ddcde855192d66ca778d8de28478dfbd
yizhi0129 <145779275+yizhi0129@users.noreply.github.com>
2023-12-15 11:40:16 +0100

    Init project

---
+++ Source/CozyGameJam2023/CozyGameJam2023.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CozyGameJam2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CozyGameJam2023, "CozyGameJam2023" );
+
+DEFINE_LOG_CATEGORY(LogCozyGameJam2023)
+ 
